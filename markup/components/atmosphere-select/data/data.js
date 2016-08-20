9302ab470165fdf062537dd697823161f784fda2
extiser <1sappho@gmail.com>
2016-08-20 16:50:13 +0500

    добавлен новый компомент atmosphere-select, адаптация atmopsphere-select

---
+++ markup/components/atmosphere-select/data/data.js
@@ -0,0 +1,56 @@
+var data = {
+  select: [
+    {
+        name: 'SPINNING',
+        link: '#'
+    },
+    {
+        name: 'IZONE',
+        link: '#'
+    },
+    {
+        name: 'CROSSFIT',
+        link: '#'
+    },
+    {
+        name: 'TRX',
+        link: '#'
+    },
+    {
+        name: 'COMBAT ZONE',
+        link: '#'
+    },
+    {
+        name: 'MIND & BODY ZONE',
+        link: '#'
+    },
+    {
+        name: 'SPA',
+        link: '#'
+    },
+    {
+        name: 'PERSONAL TRENINGS',
+        link: '#'
+    },
+    {
+        name: 'MASSAGE',
+        link: '#'
+    },
+    {
+        name: 'Nutritionist',
+        link: '#'
+    },
+    {
+        name: 'TANITA',
+        link: '#'
+    },
+    {
+        name: 'KIDS ZONE',
+        link: '#'
+    },
+    {
+        name: 'SOLARIUM',
+        link: '#'
+    }
+  ]
+}
