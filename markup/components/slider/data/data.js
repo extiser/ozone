70e9756dfa9188f5cbc8ece463db8f13f17bbda2
extiser <1sappho@gmail.com>
2016-08-20 14:38:20 +0500

    новый компонент slider. адаптация slider

--- markup/components/slider/data/data.js
+++ markup/components/slider/data/data.js
@@ -1,1 +1,16 @@
-var data = {slider: {}}
+var data = {
+  main: [
+    {
+      img: '%=static=%images/assets/slider/main-slide-1.jpg',
+      text: 'Идеальная атмосфера для здорового и активного образа жизни',
+      link: '#',
+      button: 'подробнее'
+    },
+    {
+      img: '%=static=%images/assets/slider/main-slide-2.jpg',
+      text: 'Занимайтесь спортом для здорового тела и духа',
+      link: '#',
+      button: 'подробнее'
+    }
+  ]
+}
---
+++ markup/components/slider/slider.js
@@ -0,0 +1,10 @@
+$('.jsSlider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  speed: 600,
+  fade: true,
+  cssEase: 'linear',
+  arrows: false
+});
