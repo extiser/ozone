c71a3ceb5cf2e6c088a17a93988012e2a0c1bf40
extiser <1sappho@gmail.com>
2016-08-21 13:41:56 +0500

    изменены иконки на font-awesome, добавлен новый компонент footer

---
+++ markup/components/footer/data/data.js
@@ -0,0 +1,33 @@
+data = {
+  pages: [
+  {
+    title: 'о нас',
+    slug: '#',
+    active: false
+  },
+  {
+    title: 'клубы',
+    slug: '#',
+    active: true
+  },
+  {
+    title: 'сервисы',
+    slug: '#',
+    active: false
+  },
+  {
+    title: 'команда',
+    slug: '#',
+    active: false
+  },
+  {
+    title: 'member me',
+    slug: '#',
+    active: false
+  },
+  {
+    title: 'контакты',
+    slug: '#',
+    active: false
+  }
+]};
